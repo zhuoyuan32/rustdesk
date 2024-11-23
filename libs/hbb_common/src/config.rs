bed3381298931c94f0fac33191329c617f1b8d55
zhuoyuan32 <57480334+zhuoyuan32@users.noreply.github.com>
2024-11-24 00:34:49 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.zhuoyuan32.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "oHjMdehOrB+VerIkKLM0kIpHkQ38n8tsAMbIFCnQ4BE=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ayuange.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "bJUifFPhB2LibxgpKDtaTUnQxxoAUaxcss6GST9fbag=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
